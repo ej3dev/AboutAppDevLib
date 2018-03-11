d32958d025bbafe8509f78eb31dcc95f916d5293
ej3 <ej3dev@gmail.com>
2018-03-11 04:25:43 +0100

    Added app/lib icon. Removed default launcher icons

--- TODO.md
+++ TODO.md
@@ -2,6 +2,9 @@
 # AboutAppDevLib TODO
 #
 
+### Su.11-Mar-2018
+- [ ] Add methods with resource ID parameters in ButtonFactory
+
 ### Sa.10-Mar-2018
 - [ ] Add constructor (ctx,json) to AppBuilder, DevBuilder and LibBuilder classes
 
--- app/src/main/java/net/ej3/libs/aboutappdevlib/app/DemoActivity.java
+++ app/src/main/java/net/ej3/libs/aboutappdevlib/app/DemoActivity.java
@@ -109,9 +109,9 @@ public class DemoActivity extends AppCompatActivity {
             .withHasStableIds(true)
             .withSelectedItem(-1)
             .addDrawerItems(
-                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_1).withName("Example 1").withIcon(GoogleMaterial.Icon.gmd_account_circle).withSelectable(false),
-                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_2).withName("Example 2").withIcon(GoogleMaterial.Icon.gmd_check_box).withSelectable(false),
-                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_3).withName("Example 3").withIcon(GoogleMaterial.Icon.gmd_check_box).withSelectable(false),
+                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_1).withName("Home").withIcon(GoogleMaterial.Icon.gmd_home).withSelectable(false),
+                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_2).withName("Example 1").withIcon(GoogleMaterial.Icon.gmd_filter_1).withSelectable(false),
+                new PrimaryDrawerItem().withIdentifier(DRAWER_EXAMPLE_3).withName("Example 2").withIcon(GoogleMaterial.Icon.gmd_filter_2).withSelectable(false),
                 new DividerDrawerItem(),
                 new PrimaryDrawerItem().withIdentifier(DRAWER_ABOUT).withName("About").withIcon(GoogleMaterial.Icon.gmd_info).withSelectable(false)
             )
@@ -149,7 +149,7 @@ public class DemoActivity extends AppCompatActivity {
     }
 
     private Fragment buildExample1Fragment() {
-        Fragment appFragment = new AboutAppFragment.Builder()
+        Fragment appFragment = new AboutAppFragment.Builder(this)
             .withIcon(R.drawable.ic_launcher)
             .withName(getString(R.string.about_app_name))
             .withVersion(getString(R.string.about_app_version,BuildConfig.VERSION_NAME,BuildConfig.VERSION_CODE))
@@ -172,17 +172,15 @@ public class DemoActivity extends AppCompatActivity {
                 .withTabsMode(TabLayout.MODE_SCROLLABLE)
                 .withTabsBackgroundColor(ContextCompat.getColor(this,R.color.md_blue_grey_600))
                 .addPage("APP",appFragment)
-                .addPage("DEV",new AboutDevFragment.Builder().build())
-                .addPage("LIB",new AboutLibFragment.Builder().build())
-                .addPage(R.mipmap.ic_launcher,new FooFragment())
-                .addPage(R.mipmap.ic_launcher_round,new FooFragment())
+                .addPage("DEV",new AboutDevFragment.Builder(this).build())
+                .addPage("LIB",new AboutLibFragment.Builder(this).build())
                 .addPage("FOUR",new FooFragment())
                 .addPage("FIVE",new FooFragment())
                 .build();
     }
 
     private Fragment buildAboutFragment() {
-        Fragment appFragment = new AboutAppFragment.Builder()
+        Fragment appFragment = new AboutAppFragment.Builder(this)
             .withIcon(R.drawable.ic_launcher)
             .withName(getString(R.string.about_app_name))
             .withVersion(getString(R.string.about_app_version,BuildConfig.VERSION_NAME,BuildConfig.VERSION_CODE))
@@ -190,29 +188,30 @@ public class DemoActivity extends AppCompatActivity {
             .withThanks(getString(R.string.about_app_thanks_title),getString(R.string.about_app_thanks_text))
             .withChangelog(getString(R.string.about_app_changelog_title),getString(R.string.about_app_changelog_text))
             .withActions(
-                ButtonFactory.openGithub(this,"ej3dev"),
-                ButtonFactory.openBitbucket(this,"ej3dev")
+                ButtonFactory.openGithub(this,getString(R.string.about_dev_github)),
+                ButtonFactory.openBitbucket(this,getString(R.string.about_dev_github))
             )
             .build();
 
-        Fragment devFragment = new AboutDevFragment.Builder()
+        Fragment devFragment = new AboutDevFragment.Builder(this)
             .withLogo(R.drawable.icon_dev)
-            .withAuthor("E.J. Jiménez")
-            .withInfo("Salamanca (Spain)")
+            .withAuthor(R.string.about_dev_author)
+            .withInfo(R.string.about_dev_info)
             .withActions(
-                ButtonFactory.openPlayStoreDev(this,"5497160455081780210")
+                ButtonFactory.openPlayStoreDev(this,getString(R.string.about_dev_store))
             )
             .withApps(getString(R.string.about_dev_other_apps),
-                new AppBuilder(this,R.string.about_dev_rea_name).withUrlOrPackageName("https://play.google.com/store/apps/details?id=net.ej3.apps.reminderalarm").withDescription(R.string.about_dev_rea_description).withIcon(R.drawable.icon_app_rea).build(),
+                new AppBuilder(this,R.string.about_dev_rea_name).withUrlOrPackageName(R.string.about_dev_rea_package).withDescription(R.string.about_dev_rea_description).withIcon(R.drawable.icon_app_rea).build(),
                 new AppBuilder(this,R.string.about_dev_nac_name).withUrlOrPackageName(R.string.about_dev_nac_package).withDescription(R.string.about_dev_nac_description).withIcon(R.drawable.icon_app_nac).build(),
                 new AppBuilder(this,R.string.about_dev_aps_name).withUrlOrPackageName(R.string.about_dev_aps_package).withDescription(R.string.about_dev_aps_description).withIcon(R.drawable.icon_app_aps).build(),
                 new AppBuilder(this,R.string.about_dev_sqr_name).withUrlOrPackageName(R.string.about_dev_sqr_package).withDescription(R.string.about_dev_sqr_description).withIcon(R.drawable.icon_app_sqr).build()
             )
             .build();
 
-        Fragment libFragment = new AboutLibFragment.Builder()
+        Fragment libFragment = new AboutLibFragment.Builder(this)
             .withInfo(getString(R.string.about_lib_info))
             .withLibs(getString(R.string.about_lib_libraries),
+                new LibBuilder(this,R.string.about_lib_aboutappdevlib_name).withAuthor(R.string.about_lib_aboutappdevlib_author).withDescription(R.string.about_lib_aboutappdevlib_description).withUrl(R.string.about_lib_aboutappdevlib_url).withIcon(R.drawable.icon_lib_aboutappdevlib).build(),
                 new LibBuilder(this,R.string.about_lib_material_drawer_name).withAuthor(R.string.about_lib_material_drawer_author).withDescription(R.string.about_lib_material_drawer_description).withUrl(R.string.about_lib_material_drawer_url).build(),
                 new LibBuilder(this,R.string.about_lib_logger_name).withAuthor(R.string.about_lib_logger_author).withDescription(R.string.about_lib_logger_description).withUrl(R.string.about_lib_logger_url).build(),
                 new LibBuilder(this,R.string.about_lib_leak_canary_name).withAuthor(R.string.about_lib_leak_canary_author).withDescription(R.string.about_lib_leak_canary_description).withUrl(R.string.about_lib_leak_canary_url).withIcon(R.drawable.leak_canary_icon).build()
@@ -220,7 +219,7 @@ public class DemoActivity extends AppCompatActivity {
             .build();
 
         return new AboutFragment.Builder()
-            .withTabsMode(TabLayout.MODE_SCROLLABLE)
+            .withTabsMode(TabLayout.MODE_FIXED)
             .withTabsBackgroundColor(ContextCompat.getColor(this,R.color.md_blue_grey_600))
             .addPage(getString(R.string.about_app_tab),appFragment)
             .addPage(getString(R.string.about_dev_tab),devFragment)
